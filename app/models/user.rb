8b320f50ea3908f297641231bdc8e6d7c2385d73
kurotora69 <yuka.bftt@gmail.com>
2022-12-05 15:32:19 +0900

    Fakerの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, presence: true
-  validates :password, presence: true, 
+  validates :password, presence: true
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,52 +6,69 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+    it 'nickname、email、password、password_confirmation、first_name、last_name、first_name_read、last_name_read、birthdayが存在すれば登録できる' do
+
+    end
+
     it 'nicknameが空では登録できない' do
       @user.nickname = ''
       @user.valid?
-      expect(user.errors.full_messages).to include("Nickname can't be blank")
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
     end
 
     it 'emailが空では登録できない' do
       @user.email = ''
       @user.valid?
-      expect(user.errors.full_messages).to include("Email can't be blank")
+      expect(@user.errors.full_messages).to include("Email can't be blank")
     end
 
     it 'passwordが空では登録できない' do
       @user.password = ''
       @user.valid?
-      expect(user.errors.full_messages).to include("Password can't be blank")
+      expect(@user.errors.full_messages).to include("Password can't be blank")
     end
 
     it 'first_nameが空では登録できない' do
       @user.first_name = ''
       @user.valid?
-      expect(user.errors.full_messages).to include("First_name can't be blank")
+      expect(@user.errors.full_messages).to include("First_name can't be blank")
     end
 
     it 'last_nameが空では登録できない' do
       @user.last_name = ''
       @user.valid?
-      expect(user.errors.full_messages).to include("Last_name can't be blank")
+      expect(@user.errors.full_messages).to include("Last_name can't be blank")
     end
 
     it 'first_name_readが空では登録できない' do
       @user.first_name_read = ''
       @user.valid?
-      expect(user.errors.full_messages).to include("First_name_read can't be blank")
+      expect(@user.errors.full_messages).to include("First_name_read can't be blank")
     end
 
     it 'last_name_readが空では登録できない' do
       @user.last_name_read = ''
       @user.valid?
-      expect(user.errors.full_messages).to include("Last_name_read can't be blank")
+      expect(@user.errors.full_messages).to include("Last_name_read can't be blank")
     end
 
     it 'birthdayが空では登録できない' do
       @user.birthday = ''
       @user.valid?
-      expect(user.errors.full_messages).to include("Birthday can't be blank")
+      expect(@user.errors.full_messages).to include("Birthday can't be blank")
+
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+      end
+
+      it '重複したemailが存在する場合は登録できない' do
+      end
+
+      it 'emailは@を含まないと登録できない' do
+      end
+      it 'passwordが5文字以下では登録できない' do
+      end
+      it 'passwordが129文字以上では登録できない' do
+      end
     end
   end
 end
