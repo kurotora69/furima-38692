89864d84a9a56e17ecd7224cfd630229ef7de4a6
kurotora69 <yuka.bftt@gmail.com>
2022-12-06 18:30:39 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,16 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
   end
   
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :category_id, :detail_id, :delivery_charge_id, :number_day_id, :price, :explanation, :user, :image).merge(user_id: current_user.id)
+  end
 end
