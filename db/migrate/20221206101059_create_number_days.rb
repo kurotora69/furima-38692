df6c1893ba22233a4964f29e724b34eaa9330720
kurotora69 <yuka.bftt@gmail.com>
2022-12-07 15:21:12 +0900

    ActiveHash用モデルの作り直し

--- db/migrate/20221206101059_create_number_days.rb
+++
@@ -1,8 +0,0 @@
-class CreateNumberDays < ActiveRecord::Migration[6.0]
-  def change
-    create_table :number_days do |t|
-
-      t.timestamps
-    end
-  end
-end
--- spec/factories/number_days.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :number_day do
-    
-  end
-end
