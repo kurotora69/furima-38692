ece8d40f0ca81a8392ef211e4f20196acc982a27
kurotora69 <yuka.bftt@gmail.com>
2022-12-13 15:15:08 +0900

    orderモデルにバリデーション記述

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,19 @@
 class Orders
   include ActiveModel::Model
-  attr_accessor :users_item, :delivery_charge_id, :price, :post_code, :prefecture_id, :city, :address, :building, :tel
+  attr_accessor :users_item, :delivery_charge_id, :price, :post_code, :prefecture_id, :city, :address, :building, :create_table, :user, :item
+
+  with_options presence: true do
+    validates :user
+    validates :item
+    validates :post_code
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :address
+    validates :tel
+    validates :building
+    validates :users_item
+  end  
+
+  def save
+    # 各テーブルにデータを保存する処理を書く
+  end  
