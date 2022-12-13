b149511cf1b53afed06c08af2d6dc95f72837579
kurotora69 <yuka.bftt@gmail.com>
2022-12-13 18:25:46 +0900

    バリデーション修正、saveメソッド追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,11 @@
 class Order
   include ActiveModel::Model
-  attr_accessor :users_item, :delivery_charge_id, :price, :post_code, :prefecture_id, :city, :address, :building, :create_table, :user, :item
+  attr_accessor :users_item, :price, :post_code, :prefecture_id, :city, :address, :building, :tel, :user, :item
 
   with_options presence: true do
     validates :user
     validates :item
-    validates :post_code,     format: { with: /^[0-9]{3}-[0-9]{4}$/ }
+    validates :post_code,     format: { with: /\A\d{3}[-]\d{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address
@@ -14,9 +14,11 @@ class Order
   end  
   validates :building
 
-  # def save
-  #   UsersAddress.create(users_address: )
-  #   UsersItem.create()
-  # end 
+  def save
+    UsersAddress.create(users_item_id: user_item_id, price: price, post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building: building, tel: tel)
+    UsersItem.create(user: user, item: item)
+  end 
 end  
+
+
 
