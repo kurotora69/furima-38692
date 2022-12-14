b0306f6903de3d366e0ece4ed3d882e300b16ea3
kurotora69 <yuka.bftt@gmail.com>
2022-12-14 12:58:21 +0900

    商品購入ビューの編集、turbolinks削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  # before_action :order_params, only: :create
+
 
   def index
     @item = Item.find(params[:item_id])
@@ -10,6 +12,7 @@ class OrdersController < ApplicationController
   
   def create
     # binding.pry
+    @item = Item.find(params[:item_id])
     @order = Order.new(order_params)
     if @order.valid?
       @order.save
@@ -20,13 +23,13 @@ class OrdersController < ApplicationController
   end
 
   private
-  def set_params
-    params.require(:post_code, :prefecture_id, :city, :address, :building, :tel, :user_id, item_id ).merge(user_id: current_user.id, item_id: params[:item_id], users_item: params[:item_id])
-  end
-  # :user, :item,:users_itemはpermitに必要？permit
-
-
   def order_params
+    params.require(:order).permit(:post_code, :prefecture_id, :city, :address, :building, :tel).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+  # :user, :item,:users_itemはpermitに必要？permit  :user_id, :item_id
+  # 
+
+  def item_params
     params.require(:item).permit(:item_name, :category_id, :detail_id, :delivery_charge_id, :number_day_id, :prefecture_id,
                                  :price, :explanation, :image).merge(user_id: current_user.id)
   end
